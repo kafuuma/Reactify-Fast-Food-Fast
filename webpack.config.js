1aef40d015617d5953ac2ada32a1b3ae9d678fec
kafuuma <arkafuuma@gmail.com>
2019-02-07 14:27:40 +0300

    chore(Host to Heroku): Host the app to heroku
    This commit delivers a chore to host the applicaion
    to heroku
    
    [Delivers #163726200]

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,62 @@
+// const path = require('path');
+// const webpack = require('webpack');
+
+// module.exports = {
+//   entry: './src/index.jsx',
+//   mode: 'development',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /(node_modules|bower_components)/,
+//         loader: 'babel-loader',
+//         options: { presets: ['@babel/env'] }
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader']
+//       },
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: ['eslint-loader', 'babel-loader']
+//       },
+//       {
+//         test: /\.(gif|png|jpe?g|svg)$/i,
+//         use: [
+//           'file-loader',
+//           {
+//             loader: 'image-webpack-loader',
+//             options: {
+//               bypassOnDebug: true, // webpack@1.x
+//               disable: true // webpack@2.x and newer
+//             }
+//           }
+//         ]
+//       }
+//     ]
+//   },
+//   resolve: { extensions: ['*', '.js', '.jsx'] },
+//   output: {
+//     path: path.resolve(__dirname, 'dist/'),
+//     publicPath: '/dist/',
+//     filename: 'bundle.js'
+//   },
+//   devServer: {
+//     contentBase: path.join(__dirname, 'public/'),
+//     port: 3000,
+//     publicPath: 'http://localhost:3000/dist/',
+//     hotOnly: true,
+//     historyApiFallback: true
+//   },
+//   plugins: [new webpack.HotModuleReplacementPlugin()],
+//   node: {
+//     fs: 'empty'
+//   }
+// };
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
@@ -9,28 +66,24 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: { presets: ['@babel/env'] }
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
       },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['eslint-loader', 'babel-loader']
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
         use: [
-          'file-loader',
           {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true // webpack@2.x and newer
-            }
+            loader: 'file-loader',
+            options: {}
           }
         ]
       }
@@ -39,18 +92,18 @@ module.exports = {
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
     path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
+    publicPath: '/',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public/'),
     port: 3000,
-    publicPath: 'http://localhost:3000/dist/',
-    hotOnly: true,
     historyApiFallback: true
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-  node: {
-    fs: 'empty'
-  }
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: './index.html'
+    })
+  ]
 };
